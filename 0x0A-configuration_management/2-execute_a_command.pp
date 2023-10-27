47a5dbb4fc026f83b3af671bd9f3a4a0d65b5e72
TrudyB-T <drtrudybannerman@gmail.com>
2023-10-27 05:27:33 -0700

    execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # kills a process named killmenow
-exec { 'pkill':
-command   => 'pkill killmenow',
-provider  => 'shell',
-returns   => [0, 1],
+exec { 'pkill killmenow':
+  path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+  returns => [0, 1],
 }
